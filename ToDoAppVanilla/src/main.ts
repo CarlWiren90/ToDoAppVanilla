fcfc8ff1c2b1d029b32f84ed0345bbf1a1ed2cbd
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2024-10-06 15:47:50 +0200

    Edit tasks fixed
    Fixed so that editing tasks is now possible.

--- ToDoAppVanilla/src/main.ts
+++ ToDoAppVanilla/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 
 
 //Task list
-const tasks:any = [];
+const tasks:Task[] = [];
 let taskIDNumber = 0;
 let tasksCompleted = 0;
 
@@ -18,14 +18,14 @@ class Task {
 
 
 
-const todoContainer: HTMLElement = document.getElementById("todo-container");
+const todoContainer: HTMLElement | Node | null = document.getElementById("todo-container");
 let taskItem: HTMLElement = document.createElement('span');
 let taskContainer: HTMLElement = document.createElement("div");
-let taskInput = document.createElement('input');
-let taskInputValue: string;
-let taskEditInputValue: any;
-const addTaskButton: HTMLElement = document.getElementById('addTaskButton');
-const saveTaskButton: HTMLElement = document.getElementById('saveTaskButton');
+let taskInput: Node | HTMLElement | string = document.createElement('input');
+let taskInputValue: Node | HTMLElement | string;
+let taskEditInputValue: Node | HTMLElement | string;
+const addTaskButton: HTMLElement | null | Node = document.getElementById('addTaskButton');
+const saveTaskButton: HTMLElement | null = document.getElementById('saveTaskButton');
 
 
 const renderTaskList = () => {
@@ -34,6 +34,7 @@ const renderTaskList = () => {
         //Creates task container
         taskContainer = document.createElement("div");
         taskContainer.className = 'task-container';
+        taskContainer.id = 'task-container' + tasks.indexOf(tasks[i]);
         todoContainer.appendChild(taskContainer);
 
         taskItem = document.createElement('span');
@@ -42,11 +43,12 @@ const renderTaskList = () => {
         taskContainer.appendChild(taskItem); 
         taskItem.innerHTML = taskName; 
         console.log(tasks[i].taskID)
+        console.log(tasks);
         renderTaskButtons(tasks[i].taskID);        
     }
 }
 
-const renderTaskButtons = (taskItemID: string) => {
+const renderTaskButtons = (taskItemID: number) => {
     let buttonsContainer = document.createElement('div');
     buttonsContainer.className = 'buttons-container';
     taskContainer.appendChild(buttonsContainer);
@@ -67,13 +69,13 @@ const renderTaskButtons = (taskItemID: string) => {
     buttonsContainer.appendChild(checkmarkButton);
 }
 
-const completeTask = (taskItemID: string) => {
+const completeTask = (taskItemID: number) => {
 for (let i: number = 0 ; i < tasks.length ; i ++ ) {
         if (tasks[i].taskID === taskItemID) {
             let indexCompletedTask = tasks.indexOf(tasks[i]);
             tasks.splice(indexCompletedTask, 1);
-            console.log(`deleted ${taskItemID}`);
-            console.log(tasks);
+/*             console.log(`deleted ${taskItemID}`);
+ */            console.log(tasks);
             taskIDNumber--;
             tasksCompleted++;
         }
@@ -82,11 +84,40 @@ for (let i: number = 0 ; i < tasks.length ; i ++ ) {
     renderTaskList();
 }
 
-const editTask = (taskItemID: string) => {
+const editTask = (taskItemID: number) => {
+    for (let i:number = 0 ; i < tasks.length ; i ++) {
+        if (tasks[i].taskID === taskItemID) {
+            let taskContainerToBeRemoved = document.getElementById('task-container' + tasks.indexOf(tasks[i]));
+            let editTaskContainer = document.createElement('div');
+            editTaskContainer.id ='editTaskContainer';
+            taskContainerToBeRemoved.replaceWith(editTaskContainer);
 
+            let saveEditButton = document.createElement('button');
+            saveEditButton.id = 'saveEditButton';
+            saveEditButton.innerText = 'Save!';
+            editTaskContainer.appendChild(saveEditButton);
+            
+            let newEditInputForm = document.createElement('input');
+            newEditInputForm.id = 'newEditInputForm';
+            newEditInputForm.value = tasks[i].taskName;
+            editTaskContainer.appendChild(newEditInputForm);
+
+            saveEditButton.addEventListener('click', () => {
+                console.log('save clicked');               
+                let newEditedTask = new Task(newEditInputForm.value, tasks.indexOf(tasks[i]));
+                let taskToBeEditedIndex = tasks.indexOf(tasks[i]);
+                tasks.splice(taskToBeEditedIndex, 1);
+                tasks.splice(taskToBeEditedIndex, 0, newEditedTask);
+                editTaskContainer.removeChild(newEditInputForm);
+                todoContainer?.removeChild(editTaskContainer);
+                renderTaskList();
+                console.log(tasks);
+            })
+        }
+    }
 };
 
-const deleteTask = (taskItemID: string) => {
+const deleteTask = (taskItemID: number) => {
     for (let i: number = 0 ; i < tasks.length ; i++ ) {
         if(tasks[i].taskID === taskItemID) {
             let indexDeletedTask = tasks.indexOf(tasks[i]);
@@ -103,10 +134,8 @@ const updateTasksCompleted = () => {
 
 
 todoContainer.addEventListener('click', () => {
-    console.log('clicked');
     taskItem.addEventListener('click',  () => {
-        let taskToBeEdited = document. getElementById('task-item');
-        console.log(taskToBeEdited);
+        let taskToBeEdited: HTMLElement | null = document. getElementById('task-item');
     })
 });  
 
@@ -123,8 +152,7 @@ addTaskButton.addEventListener('click', () => {
 
 saveTaskButton.addEventListener('click', () => {
 
-    let taskInputValue = taskInput.value;
-
+    taskInputValue = taskInput.value;
     if (taskInputValue === '' || null ) {
         taskInput.placeholder = "Enter task here!";
     }
